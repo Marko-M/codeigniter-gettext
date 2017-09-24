2f42b87d336cf90c9b5bb3e47eda51d73a08388c
Joël Gaujard <j.gaujard@gmail.com>
2017-09-24 23:42:29 +0200

    More simple tests for helper

--- tests/HelperTest.php
+++ tests/HelperTest.php
@@ -6,34 +6,22 @@ class HelperTest extends \PHPUnit_Framework_TestCase
     const EXPRESSION = "Let me test this expression";
     const EXPRESSION_PLURAL = "Let me test this plural expression";
 
-    public function testDoubleUnderscore_Exits()
-    {
-        $this->assertTrue(function_exists('__'), "__ function do not exists.");
-    }
-
     public function testDoubleUnderscore()
     {
+        $this->assertTrue(function_exists('__'), "__ function do not exists.");
         $this->assertEquals(self::EXPRESSION, __(self::EXPRESSION));
     }
 
-    public function testUnderscoreE_Exits()
+    public function testUnderscoreE()
     {
         $this->assertTrue(function_exists('_e'), "_e function do not exists.");
-    }
-
-    public function testUnderscroreE()
-    {
         $this->expectOutputRegex('/' . self::EXPRESSION . '/');
         _e(self::EXPRESSION);
     }
 
-    public function testUnderscoreN_Exits()
-    {
-        $this->assertTrue(function_exists('_n'), "_e function do not exists.");
-    }
-
     public function testUnderscroreN_Singular()
     {
+        $this->assertTrue(function_exists('_n'), "_e function do not exists.");
         $this->assertEquals(
             self::EXPRESSION,
             _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 1)
@@ -42,6 +30,7 @@ class HelperTest extends \PHPUnit_Framework_TestCase
 
     public function testUnderscroreN_Plural()
     {
+        $this->assertTrue(function_exists('_n'), "_e function do not exists.");
         $this->assertEquals(
             self::EXPRESSION_PLURAL,
             _n(self::EXPRESSION, self::EXPRESSION_PLURAL, 2)
